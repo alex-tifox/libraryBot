01c7b0a7cb134969bc37f73a30bfed56a210bd71
mrF0x729 <tilnyi.olexander@gmail.com>
2018-07-18 19:15:09 +0300

    webhook added

--- src/bot.py
+++ src/bot.py
@@ -5,9 +5,20 @@ import os
 import config
 import telebot
 import book_dictionary
+import cherrypy
 from telebot import types
 
 bot = telebot.TeleBot(config.token)
+
+WEBHOOK_HOST = '185.103.110.156'
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = '../webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = '../webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (config.token)
 
 
 @bot.message_handler(commands=['start'])
--- src/config.py
+++ src/config.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 token = "614182466:AAG1jgDM6Ho4zDI_7UMz7hwg_VvHNrO_meU"
+
