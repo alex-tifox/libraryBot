fb0e1e357f67595b28a81be096c08aea8796be0e
mrF0x729 <tilnyi.olexander@gmail.com>
2018-07-18 19:39:50 +0300

    cherrypy config + quickstart polling removed

--- src/bot.py
+++ src/bot.py
@@ -103,4 +103,19 @@ def handle_start(message):
 			print(os.path.join(directory, book))
 
 
-bot.polling(none_stop=True)
+bot.remove_webhook()
+
+ # Ставим заново вебхук
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+cherrypy.config.update({
+    'server.socket_host': WEBHOOK_LISTEN,
+    'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
