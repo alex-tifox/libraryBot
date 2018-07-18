1df9c0c289bd1dacebe2b52b0d65a1ce5e764e35
mrF0x729 <tilnyi.olexander@gmail.com>
2018-07-18 19:38:17 +0300

    webhook class

--- src/bot.py
+++ src/bot.py
@@ -20,6 +20,20 @@ WEBHOOK_SSL_PRIV = '../webhook_pkey.pem'  # Путь к приватному к�
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (config.token)
 
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
